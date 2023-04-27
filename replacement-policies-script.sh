1c691be761cd76fce503e00acf0515b75e9d36e6
ManasiIngle <111419673+ManasiIngle@users.noreply.github.com>
2023-04-27 11:58:42 +0530

    Add files via upload

---
+++ replacement-policies-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./run_champsim.sh bimodal-no-no-no-no-lru-1core 30 30 1.champsimtrace.xz option
+./run_champsim.sh bimodal-no-no-no-no-lru-1core 30 30 2.champsimtrace.xz option
+./run_champsim.sh bimodal-no-no-no-no-lru-1core 30 30 3.champsimtrace.xz option
+./run_champsim.sh bimodal-no-no-no-no-lru-1core 30 30 4.champsimtrace.xz option
+
+
+
